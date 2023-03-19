7bc8c5da16aa1cd6402407dd72414666b1232567
TanyaMaximova1 <tatamurom@gmail.com>
2023-03-20 01:46:09 +0300

    step

---
+++ index.js
@@ -0,0 +1,13 @@
+// fetch("localhost/pets/add",
+//     {
+//         method: 'POST',
+//         body: JSON.stringify(параметр),
+//         headers: {
+//             'Content-Type': 'application/json; charset=utf8'
+//         },
+//     })
+//     .then(response => response.json())
+//     .then(параметр => {
+//         console.log(параметр);
+//     })
+//     .catch(error => console.error());

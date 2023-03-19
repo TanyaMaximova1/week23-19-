0430231f80afb79961f6588e5c462555c622661d
TanyaMaximova1 <tatamurom@gmail.com>
2023-03-20 02:11:03 +0300

    step

--- index.js
+++ index.js
@@ -1,13 +1,18 @@
-// fetch("localhost/pets/add",
-//     {
-//         method: 'POST',
-//         body: JSON.stringify(параметр),
-//         headers: {
-//             'Content-Type': 'application/json; charset=utf8'
-//         },
-//     })
-//     .then(response => response.json())
-//     .then(параметр => {
-//         console.log(параметр);
-//     })
-//     .catch(error => console.error());
+// postButton.onclick = function(e) {
+//     e.prevent
+// }
+
+// как сделать POST-запрос, чтобы создать пост
+fetch('https://jsonplaceholder.typicode.com/posts', {
+    method: 'POST',
+    body: JSON.stringify({
+        title: 'foo',
+        body: 'bar',
+        userId: 1
+    }),
+    headers: {
+        "Content-type": "application/json; charset=UTF-8"
+    }
+})
+    .then(response => response.json())
+    .then(json => console.log(json))

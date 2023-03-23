584bdbca185f6b9ac28adb130f76989fac1eb762
TanyaMaximova1 <tatamurom@gmail.com>
2023-03-24 00:01:48 +0300

    step2

--- index.js
+++ index.js
@@ -2,29 +2,46 @@
 //     e.prevent
 // }
 
+//задание 2
 // как сделать POST-запрос, чтобы создать пост
-// fetch('https://jsonplaceholder.typicode.com/posts', {
-//     method: 'POST',
-//     body: JSON.stringify({
-//         title: 'foo',
-//         body: 'bar',
-//         userId: 1
-//     }),
-//     headers: {
-//         "Content-type": "application/json; charset=UTF-8"
-//     }
-// })
-//     .then(response => response.json())
-//     .then(json => console.log(json))
 
-function getPosts(posts) {
-    fetch('https://jsonplaceholder.typicode.com/todos/1')
+function createPost() {
+    let post = {
+        title: document.getElementById("titlePost").value,
+        text: document.getElementById("textPost").value
+    }
+
+    fetch('https://jsonplaceholder.typicode.com/posts', {
+        method: 'POST',
+        body: JSON.stringify(post),
+        headers: {
+            "Content-type": "application/json; charset=UTF-8"
+        },
+    })
         .then(response => response.json())
-        .then(posts => {
-            console.log(posts);
-            document.getElementsByClassName("container").innerText = posts;
+        .then(post => {
+            console.log(post);
+            document.getElementById("container").innerText = post;
         })
         .catch(err => console.log(err));
 }
+
+
+
+//задание 1
+// function getPosts(posts) {
+//     fetch('https://jsonplaceholder.typicode.com/todos/1')
+//         .then(response => response.json())
+//         .then(posts => {
+//             console.log(posts);
+//             document.getElementsByClassName("container").innerText = posts;
+//         })
+//         .catch(err => console.log(err));
+// }
+// getPosts();
+
+// function postPosts() {
+
+// }
 
 

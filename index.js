1b874f04b0fae4bf9e0ad69277b74582d5748d0b
TanyaMaximova1 <tatamurom@gmail.com>
2023-03-23 22:59:06 +0300

    step

--- index.js
+++ index.js
@@ -3,16 +3,28 @@
 // }
 
 // как сделать POST-запрос, чтобы создать пост
-fetch('https://jsonplaceholder.typicode.com/posts', {
-    method: 'POST',
-    body: JSON.stringify({
-        title: 'foo',
-        body: 'bar',
-        userId: 1
-    }),
-    headers: {
-        "Content-type": "application/json; charset=UTF-8"
-    }
-})
-    .then(response => response.json())
-    .then(json => console.log(json))
+// fetch('https://jsonplaceholder.typicode.com/posts', {
+//     method: 'POST',
+//     body: JSON.stringify({
+//         title: 'foo',
+//         body: 'bar',
+//         userId: 1
+//     }),
+//     headers: {
+//         "Content-type": "application/json; charset=UTF-8"
+//     }
+// })
+//     .then(response => response.json())
+//     .then(json => console.log(json))
+
+function getPosts(posts) {
+    fetch('https://jsonplaceholder.typicode.com/todos/1')
+        .then(response => response.json())
+        .then(posts => {
+            console.log(posts);
+            document.getElementsByClassName("container").innerText = posts;
+        })
+        .catch(err => console.log(err));
+}
+
+

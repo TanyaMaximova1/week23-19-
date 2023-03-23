4b79fc5076f0417b768b17f2755b50d871c889f6
TanyaMaximova1 <tatamurom@gmail.com>
2023-03-24 01:40:38 +0300

    step

--- index.js
+++ index.js
@@ -1,18 +1,41 @@
-//задание 1
-// function getPosts(posts) {
-//     fetch('https://jsonplaceholder.typicode.com/todos/1')
-//         .then(response => response.json())
-//         .then(posts => {
-//             console.log(posts);
-//             document.getElementsByClassName("container").innerText = posts;
-//         })
-//         .catch(err => console.log(err));
-// }
-// getPosts();
+// задание 1
+function getPosts(posts) {
+    fetch('https://jsonplaceholder.typicode.com/posts')
+        .then(response => response.json())
+        .then(posts => {
+            console.log(posts);
+            // document.getElementById("title").innerText = posts.title;
+            // document.getElementById("body").innerText = posts.body;
 
-// function postPosts() {
+            // let div1 = document.createElement('div1');
+            // div1.innerHTML = `<h2>${posts.title}</h2><br/><p>${posts.body}</p>`;
+            // document.body.append(div1);
+            console.log(posts[0].title, posts[0].body);
 
-// }
+
+
+
+            var allPosts = [];
+
+            for (var post in posts) {
+                if (posts.hasOwnProperty(post)) {
+                    allPosts.push(post);
+                }
+            }
+
+
+            allPosts.forEach((item) => {
+                console.log('Пост: ' + item)
+
+                let div1 = document.createElement('div1');
+                div1.innerHTML = `<h2>Пост</h2><br/><p>${item}</p>`;
+                document.body.append(div1);
+
+            })
+        })
+        .catch(err => console.log(err));
+}
+
 
 //задание 2
 postButton.onclick = function (e) {
@@ -34,8 +57,6 @@ postButton.onclick = function (e) {
         .then(post => {
             console.log(post);
             let div = document.createElement('div');
-            // div.innerHTML = document.getElementById("container").innerText = post.title + " " + post.text;
-
             div.innerHTML = `<h2>${post.title}</h2><br/><p>${post.text}</p>`;
             document.body.append(div);
         })
